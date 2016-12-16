be54cc015ef55ab6a149f6326101eadd48855287
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2016-12-16 01:12:04 -0500

    Fixed style issues

--- src/StreetView/ImageRetriever.py
+++ src/StreetView/ImageRetriever.py
@@ -4,50 +4,62 @@ import json
 from exception import AddressNotFoundException, MetaDataRetrievalException
 
 class ImageRetriever:
-	META_URL = 'https://maps.googleapis.com/maps/api/streetview/metadata?'
-	META_STATUS = {
-		'OK': 'OK',
-		'ZERO': 'ZERO_RESULTS',
-		'NOT_FOUND': 'NOT_FOUND',
-		'OVER_LIMIT': 'OVER_QUERY_LIMIT',
-		'DENIED': 'REQUEST_DENIED',
-		'INVALID': 'INVALID_REQUEST',
-		'UNKNOWN': 'UNKNOWN_ERROR'
-	}
+    META_URL = 'https://maps.googleapis.com/maps/api/streetview/metadata?'
+    META_STATUS = {
+        'OK': 'OK',
+        'ZERO': 'ZERO_RESULTS',
+        'NOT_FOUND': 'NOT_FOUND',
+        'OVER_LIMIT': 'OVER_QUERY_LIMIT',
+        'DENIED': 'REQUEST_DENIED',
+        'INVALID': 'INVALID_REQUEST',
+        'UNKNOWN': 'UNKNOWN_ERROR'
+    }
 
-	def __init__(self, targetDir, apiKey):
-		self.targetDir = targetDir
-		self.apiKey = apiKey
+    # Specified by Google
+    MAX_FOV = 120
 
-	def retrieve_images(self, locations = []):
-		for loc in locations:
-			try:
-				meta = self.image_meta(loc);
-			except AddressNotFoundException:
-				# Nothing here, let's move on to the next one
-				continue
+    def __init__(self, targetDir, apiKey, size = {'x': 1200, 'y': 1200},
+                 fov = MAX_FOV):
+        self.targetDir = targetDir
+        self.apiKey = apiKey
+        self.size = size
+        if (fov > self.MAX_FOV):
+            raise ValueError('Requested field-of-view (FOV) cannot exceed '
+                + self.MAX_FOV + 'degrees. If the docs say it can be more than '
+                'this, please contact a maintainer so they can fix it.')
+        self.fov = fov
 
-	def image_meta(self, location):
-		# Build the URL params
-		url = self.META_URL + 'key=' + self.apiKey
-		url = url + '&location=\'' + location + '\''
+    def retrieve_images(self, locations = []):
+        images = []
 
-		response = urllib.urlopen(url)
-		meta = json.loads(response.read())
+        for loc in locations:
+            try:
+                meta = self.image_meta(loc['coords']);
+                images.append(self.get_image(meta, loc['forward_heading']))
+            except AddressNotFoundException:
+                # Nothing here, let's move on to the next one
+                continue
 
-		if (meta['status'] == self.META_STATUS['OK']):
-			return meta
-		elif (meta['status'] == self.META_STATUS['ZERO']
-		      or meta.status == self.META_STATUS['NOT_FOUND']):
-			# Since we're only expecting lat/long pairs and not
-			# street addresses, these status values basically
-			# mean the same thing
-			raise AddressNotFoundException('No imagery available '
-						       'for ' + location)
-		else:
-			raise MetaDataRetrievalException('An error occurred '
-							 'retrieving address '
-							 'metadata. The '
-							 'error was: '
-							 + meta['status'])
+    def image_meta(self, location):
+        params = urllib.urlencode({'key': self.apiKey, 'location': location})
+        url = self.META_URL + params
+
+        response = urllib.urlopen(url)
+        meta = json.loads(response.read())
+
+        if (meta['status'] == self.META_STATUS['OK']):
+            return meta
+        elif (meta['status'] == self.META_STATUS['ZERO']
+              or meta.status == self.META_STATUS['NOT_FOUND']):
+            # Since we're only expecting lat/long pairs and not
+            # street addresses, these status values basically
+            # mean the same thing
+            raise AddressNotFoundException('No imagery available for '
+                + location)
+        else:
+            raise MetaDataRetrievalException('An error occurred retrieving '
+                'address metadata. The error was: ' + meta['status'])
+
+    def get_image(self, meta, forwardHeading):
+        panID = meta['pano_id']
 
