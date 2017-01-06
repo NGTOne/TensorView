f25dbf469da72f777e42d831628bf029e18e343a
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2017-01-05 21:00:16 -0500

    Added cache-retrieval mechanism for panorama segments in Panorama class

--- googleapi/panorama.py
+++ googleapi/panorama.py
@@ -12,13 +12,19 @@ class Panorama:
             except OSError:
                 raise OSError('Could not create directory for panorama images.')
 
-        self.slices = self.find_cached_slices()
+        self.slices, self.segments = self.read_cache()
 
-    def find_cached_slices(self):
-        return [f for f in os.listdir(self.imgDir)
-                if (os.path.isfile(os.path.join(self.imgDir, f))
-                    and re.match('^-?\d+\.\d+\.jpg$', f, re.IGNORECASE))]
-                    # Regex only matches files that are named after headings
+    def read_cache(self):
+        slices = [f for f in os.listdir(self.imgDir)
+                     if (os.path.isfile(os.path.join(self.imgDir, f))
+                     # Regex only matches files that are named after headings
+                     and re.match('^-?\d+\.\d+\.jpg$', f, re.IGNORECASE))]
+        segmentDir = os.path.join(self.imgDir, 'segments')
+        segments = [f for f in os.listdir(segmentDir)
+                        if (os.path.isfile(os.path.join(segmentDir, f))
+                        and re.match('^-?\d+\.\d+_\d{2,3}_(?:[0-8]\d|90)\.jpg',
+                                     f, re.IGNORECASE))]
+        return slices, segments
 
     def set_coords(self, coords):
         self.coords = coords
@@ -26,14 +32,32 @@ class Panorama:
     def add_slice(self, filename):
         self.slices.append(filename)
 
+    def add_segment(self, filename):
+        self.segments.append(filename)
+
     def slice_count(self):
         return len(self.slices)
+
+    def segment_count(self)
+        return len(self.segments)
 
     def headings(self):
         return [image.replace('.jpg', '') for image in self.slices]
 
+    def segment_info(self):
+        info = []
+        for segment in self.segments:
+            sliced = segment.replace('.jpg', '').split('_')
+            info.append({'heading': float(sliced[0]), 'fov': float(sliced[1]),
+                         'pitch': float(sliced[2])})
+        return info
+
     def full_slice_names(self):
         return [os.path.join(self.imgDir, image) for image in self.slices]
+
+    def full_segment_names(self):
+        return [os.path.join(self.imgDir, 'segments', image)
+                    for image in self.segments]
 
     def clean_up_slice_cache(self, headings):
         # We don't want to delete the directory because we'll likely be writing
