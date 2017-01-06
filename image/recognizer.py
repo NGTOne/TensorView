ee07b909b4951b39ec0619989fdfd398b5265fc6
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2017-01-05 20:03:33 -0500

    Fixed misnamed function call

--- image/recognizer.py
+++ image/recognizer.py
@@ -18,7 +18,7 @@ class ImageRecognizer:
         return [coords for coords in interesting if coords[1] is not None]
 
     def find_interesting_headings(self, pano):
-        headings = zip(pano.headings(), pano.full_filenames())
+        headings = zip(pano.headings(), pano.full_slice_names())
         return [heading[0] for heading in headings
                    if self.is_image_interesting(heading[1])] or None
 
