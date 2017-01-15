da304ccba7d062b79c2c4e1a0c6f30a6e90fdb9c
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2017-01-14 23:27:59 -0500

    Added script for manually verifying recognizer output

---
+++ check-image.py
@@ -0,0 +1,8 @@
+from image.tf import TFImageRecognizer
+import sys
+
+if len(sys.argv) < 5:
+    raise ValueError('Usage: python check-image.py <model-file> <label-file> <top-n> <image-file>')
+
+reco = TFImageRecognizer(sys.argv[1], sys.argv[2])
+print reco.top_n(sys.argv[4], sys.argv[3])
