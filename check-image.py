6bd625f3b29eb171e6be6846a3a22c6a29c1922e
Sebastian Lenartowicz <sebastian.lenartowicz.one@gmail.com>
2017-01-16 01:47:37 -0500

    Fixed string error in image checker

--- check-image.py
+++ check-image.py
@@ -5,4 +5,4 @@ if len(sys.argv) < 5:
     raise ValueError('Usage: python check-image.py <model-file> <label-file> <top-n> <image-file>')
 
 reco = TFImageRecognizer(sys.argv[1], sys.argv[2])
-print reco.top_n(sys.argv[4], sys.argv[3])
+print reco.top_n(sys.argv[4], int(sys.argv[3]))
